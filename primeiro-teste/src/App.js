144148726fff878fac2e3f0a47b61a666ba15212
isaribeirooo <isadoraribeiro2708@gmail.com>
2025-08-01 16:41:12 -0300

    PROJETO INICIAL

---
+++ primeiro-teste/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MEU PRIMEIRO REACT</h1>
+    </div>
+  );
+}
+
+export default App;
