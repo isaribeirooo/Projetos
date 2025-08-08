96902b0ce566675510ea0d3902c283675981ace5
isaribeirooo <isadoraribeiro2708@gmail.com>
2025-08-08 17:19:54 -0300

    Criação dos compenentes e CSS

--- primeiro-teste/src/App.js
+++ primeiro-teste/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import Frase from './components/Frase';
+import DigaMeuNome from './DigaMeuNome';
 
 function App() {
   return (
     <div className="App">
       <h1>MEU PRIMEIRO REACT</h1>
+      <Frase /> 
+      <HelloWorld />
+      <Frase /> 
+      <DigaMeuNome nome="Ana e Isa"/>
+      <p>Outro paragrafo</p>
     </div>
   );
 }
---
+++ primeiro-teste/src/components/Frase.js
@@ -0,0 +1,10 @@
+import styles from './Frase.module.css'
+function Frase(){
+    return(
+        <div>
+            <p className= {styles.paragrafo}>Componente de Frase.....</p>
+        </div>
+    )
+}
+
+export default Frase
