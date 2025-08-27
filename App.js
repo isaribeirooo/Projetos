81a6af81e47316cb63c3e8b987320dbfc65cb4e0
isaribeirooo <161508466+isaribeirooo@users.noreply.github.com>
2025-08-27 16:24:17 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Evento from './components/Evento';
+import Formulario from './components/Formulario';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Testando eventos</h1>
+      <Evento numero="1"/>
+      <Evento numero="2"/>
+      <Formulario />
+
+          </div>
+  );
+}
+
+export default App;
